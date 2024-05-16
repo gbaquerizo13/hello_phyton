74e83a392e5efa5b69ec09c6f01150bafb5ee24f
Gabriela <gabrielita21.gb@gmail.com>
2024-05-16 21:35:29 +0200

    print witch type of data is in the variable

---
+++ 02_numeric_data/README.md
@@ -0,0 +1,5 @@
+##Use numeric data types
+~~~
+Using python shell we introduce numeric commands like adding, substracting, multipliying and dividing.
+
+~~~
---
+++ 02_numeric_data/exercise2.py
@@ -0,0 +1,12 @@
+#call a function to print in console a sentence
+print("python has three numeric types: int, float and complex")
+
+#create a varable, add a value and print in console with the function
+myValue=1
+print(myValue)
+
+#this print the type os data is the variable myValue = 'int'
+print(type(myValue))
+
+#this print the value like a string and add a sentence to indicate the type of data is.
+print(str(myValue) + " is of the data type " + str(type(myValue)))
