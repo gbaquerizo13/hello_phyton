459af3169699d5183b7cc2021c12878f97e3ed75
Gabriela <gabrielita21.gb@gmail.com>
2024-05-16 21:46:34 +0200

    working with float type of data

--- 02_numeric_data/exercise2.py
+++ 02_numeric_data/exercise2.py
@@ -10,3 +10,13 @@ print(type(myValue))
 
 #this print the value like a string and add a sentence to indicate the type of data is.
 print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+#create a new variable to add new type of data =float and print in console
+otherValue=3.14
+print(otherValue)
+
+#call type function to print in console witch type is in the variable
+print(type(otherValue))
+
+#combine text and number with str() function
+print(str(otherValue) + " is of the data type " + str(type(otherValue)))
