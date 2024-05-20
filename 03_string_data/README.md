f14bb4151aa0473f389bc4e2da87566ac894426b
Gabriela <gabrielita21.gb@gmail.com>
2024-05-20 20:33:42 +0200

    <introduce the  string data type

---
+++ 03_string_data/README.md
@@ -0,0 +1,4 @@
+### Using string data type
+~~~
+write phyton code that use the String data type
+~~~
---
+++ 03_string_data/exercise3.py
@@ -0,0 +1,10 @@
+#Create a variable with string value and print in console
+myString = "This is a string."
+print(myString)
+
+#Print witch type of string there is
+print(type(myString))
+
+#convert the value of type into string with srt()
+print(myString + " is of the data type " + str(type(myString)))
+
