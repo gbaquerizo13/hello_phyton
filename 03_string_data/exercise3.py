d8f1b323fb60d2e1eae66dd487ce0eee627f208d
Gabriela <gabrielita21.gb@gmail.com>
2024-05-20 20:35:05 +0200

    introduce string concatenate

--- 03_string_data/exercise3.py
+++ 03_string_data/exercise3.py
@@ -8,3 +8,8 @@ print(type(myString))
 #convert the value of type into string with srt()
 print(myString + " is of the data type " + str(type(myString)))
 
+#Create two string to concatenated with + in a third string anda print in console.
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
