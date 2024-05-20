6614c1b8360ea03b6939dcdb45a80b681bfec45b
Gabriela <gabrielita21.gb@gmail.com>
2024-05-20 19:55:42 +0200

    introduce the bool data type

--- 02_numeric_data/exercise2.py
+++ 02_numeric_data/exercise2.py
@@ -30,3 +30,23 @@ print(type(theValue))
 
 #combine the text and number with str()
 print(str(theValue) + " is of data type " + str(type(theValue)))
+
+#introduce variable with bool data type and print in console
+value5=True
+print(value5)
+
+#print the data type of value in colsole wu¡ith function type()
+print(type(value5))
+
+#combine the text and number with str() to print what type of data is the value.
+print(str(value5) + " is of the data type " + str(type(value5)))
+
+#introduce variable with bool data type and print in console
+value6=False
+print(value6)
+
+#print de data type of value6 contain.
+print(type(value6))
+
+#With str() can mix text and number to indicate witch type of data is.
+print(str(value6) + " is of the data type " + str(type(value6)))
