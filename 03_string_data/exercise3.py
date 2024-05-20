20e5838876f46fc96c4cfdf4e7930e4c6637ec2d
Gabriela <gabrielita21.gb@gmail.com>
2024-05-20 20:47:55 +0200

    introduce input function

--- 03_string_data/exercise3.py
+++ 03_string_data/exercise3.py
@@ -8,8 +8,12 @@ print(type(myString))
 #convert the value of type into string with srt()
 print(myString + " is of the data type " + str(type(myString)))
 
-#Create two string to concatenated with + in a third string anda print in console.
+#Create two string to concatenated with + in a third string and print in console.
 firstString = "water"
 secondString = "fall"
 thirdString = firstString + secondString
 print(thirdString)
+
+#Create a variable with function input and then print in console
+name = input("What is your name? ")
+print(name)
