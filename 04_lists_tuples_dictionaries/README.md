5c3f67c24280833d763b8b8113de810d4a712d79
Gabriela <gabrielita21.gb@gmail.com>
2024-05-21 09:32:50 +0200

    introduce a list data type

---
+++ 04_lists_tuples_dictionaries/README.md
@@ -0,0 +1,5 @@
+### Working with Lists, Tuples and Dictionaries
+
+~~~
+In this lab use de list data type, tuple data type and dictionary data type
+~~~
---
+++ 04_lists_tuples_dictionaries/exercise4.py
@@ -0,0 +1,8 @@
+#1. create a list of fruits
+myFruitList = ["apple","banana","cherry"]
+
+#print in console with the function print()
+print(myFruitList)
+
+#print in colsole whta type of data is the variable with function type()
+print(type(myFruitList))
