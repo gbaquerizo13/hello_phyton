b11d5a3e4bd2387a26b9a619868aaa414618958d
Gabriela <gabrielita21.gb@gmail.com>
2024-05-21 09:40:24 +0200

    introduce the access in a list by position

--- 04_lists_tuples_dictionaries/exercise4.py
+++ 04_lists_tuples_dictionaries/exercise4.py
@@ -6,3 +6,12 @@ print(myFruitList)
 
 #print in colsole whta type of data is the variable with function type()
 print(type(myFruitList))
+
+#2. print in console by pisition the fruit apple
+print(myFruitList[0])
+
+#print in console by pisition the fruit banana
+print(myFruitList[1])
+
+#print in console by position the fruit cherry
+print(myFruitList[2])
