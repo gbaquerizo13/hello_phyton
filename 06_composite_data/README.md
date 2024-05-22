cfcffd7d0643b326a65181a512d8e8f00c205676
Gabriela <gabrielita21.gb@gmail.com>
2024-05-22 13:28:20 +0200

    copy csv file into memory

--- 06_composite_data/README.md
+++ 06_composite_data/README.md
@@ -2,6 +2,7 @@
 
 Composite data types is any type of data that comprises primitive data types
 - use: Numeric, string, list, dictionary data type.
-- use: _for_ loop, _if_ statement, _else_ statementband _import_ statement.
+- use: _for_ loop, _if_ statement, _else_ statement and _import_ statement.
+- Introduce the _with open_ syntax statement and _csv.reader()_ function
 
 
--- 06_composite_data/exercise6.py
+++ 06_composite_data/exercise6.py
@@ -3,7 +3,7 @@ import csv
 import copy
 
 #create the dictionary to use as composite type for reading the data
-myVehicle = {
+myCar = {
     "vin" : "<empty>",
     "make" : "<empty>",
     "model" : "<empty>",
@@ -15,8 +15,32 @@ myVehicle = {
 }
 
 #with for loop print the key and value in terminal of the dictionary. the {} are the item in the dictionary and .format contains what is the value insert in the {} and printing
-for key, value in myVehicle.items():
+for key, value in myCar.items():
     print("{} : {}".format(key,value))
 
 #defnie a empty list to read car inventory
-myCarlist = []
+myCarList = []
+
+with open('06_composite_data/car_fleet.csv') as csvFile:
+    csvReader = csv.reader(csvFile, delimiter=',')
+    lineCount = 0
+    for row in csvReader:
+        if lineCount == 0:
+            print(f'column names are: {",".join(row)}')
+            lineCount += 1
+        else:
+            print(f'vin: {row[0]} make {row[1]}, model: {row[2]},year: {row[3]}, range: {row[4]}, topSpeed {row[5]}, zeroSixty: {row[6]}, mileage: {row[7]}')
+            currentCar = copy.deepcopy(myCar)
+            currentCar["vin"] = row[0]
+            currentCar["make"] = row[1]
+            currentCar["model"] = row[2]
+            currentCar["year"] = row[3]
+            currentCar["range"] = row[4]
+            currentCar["topSpeed"] = row[5]
+            currentCar["zeroSixty"] = row[6]
+            currentCar["mileage"] = row[7]
+            myCarList.append(currentCar)
+            lineCount += 1
+    print(f'Processed {lineCount} lines.')
+
+    currentCar = copy.deepcopy(myCar)
